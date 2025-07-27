17f870fae0e267d7f9d8e65116cff84e4036453b
Habibur <hraasif02@gmail.com>
2025-07-27 20:36:30 +0600

    json file

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 import json 
 app=FastAPI()
 
-
-
+def load_data():
+    with open('patient.json','/r') as f:
+        data=json.load(f)
+    return data
 @app.get('/')
 def hello():
     return {'message':'hello world hiii'}
@@ -12,5 +14,11 @@ def hello():
 @app.get('/about')
 def about():
     return {'message':'aasif is a idle boy'}
+
+@app.get('/view')
+
+def view():
+    data=load_data()
+    return data
 
 
