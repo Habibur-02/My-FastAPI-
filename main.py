8f3896cbf9c6a5d6d44f75dd941a12399e46718c
Habibur <hraasif02@gmail.com>
2025-07-27 20:29:35 +0600

    hhh

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ app=FastAPI()
 
 @app.get('/')
 def hello():
-    return {'message':'hello world'}
+    return {'message':'hello world hiii'}
+
+
+@app.get('/about')
+def about():
+    return {'message':'aasif is a idle boy'}
+
 
